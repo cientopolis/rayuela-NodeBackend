450c5a43d4a1792670cf4395b8ef643bf030bc22
Nico Alvarez <nicolas.alv3@gmail.com>
2024-09-11 09:11:57 -0300

    Fix swagger prod

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-const packageJson = require('../../package.json');
 
 
 async function bootstrap() {
@@ -9,16 +8,14 @@ async function bootstrap() {
     logger: ['error', 'warn', 'log'],
   });
   app.setGlobalPrefix('api/v1');
-  // Configuración del documento Swagger
   const config = new DocumentBuilder()
     .setTitle('Rayuela backend API')
     .setDescription('API para la app de rayuela')
-    .setVersion(packageJson.version)
+    .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
-  // Habilitar el módulo Swagger en la ruta /api
   SwaggerModule.setup('docs', app, document);
   app.enableCors();
   await app.listen(3000);
