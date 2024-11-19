d23c057e1a61e340b1104e5fe24585f947ba3c95
Nico Alvarez <nicolas.alv3@gmail.com>
2024-11-19 10:20:40 -0300

    fix lint

--- src/module/project/persistence/project.schema.ts
+++ src/module/project/persistence/project.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, Schema as MongooseSchema, Types } from 'mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
 import { TimeInterval } from '../../task/entities/time-restriction.entity';
 
 export type ProjectDocument = ProjectTemplate & Document;
